376f8671b3a5b129de1c18f2f9ee6b3e8538d693
Peter Malcolm <peter.t.malcolm@gmail.com>
2012-07-03 16:51:52 -0400

    now reads and reports from Arduino, weird values

--- Arduscoplet.java
+++ Arduscoplet.java
@@ -25,20 +25,32 @@ import gnu.io.SerialPortEventListener;
 import gnu.io.PortInUseException;
 import gnu.io.UnsupportedCommOperationException;
 // for enumerating ports
+import java.io.IOException;
 import java.util.Enumeration;
+import java.util.TooManyListenersException;
 
-public class Arduscoplet {
+public class Arduscoplet implements SerialPortEventListener{
 
     private static final String PORT_NAMES[] = { 
                     "/dev/tty.usbserial-A9007UX1", // Mac OS X
                     "/dev/ttyUSB0", // Linux
                     "COM3", // Windows
+                    "COM4",
+                    "COM5",
+                    "COM6",
+                    "COM7",
+                    "COM8",
+                    "COM9"
                     };
     /** Milliseconds to block while waiting for port open */
     private static final int TIME_OUT = 2000;    
 
     private static final int DATA_RATE = 115200;  // Peter Malcolm: data rate for oscilloscope (PM)
     
+    private JFrame f = null;
+    private MyPanel p = null;
+    private InputStream in = null;
+    private OutputStream out = null;
     private SerialPort serialPort;
     
     
@@ -47,25 +59,29 @@ public class Arduscoplet {
     //////////////////////////////////////////////////////////////////////////////
     public SerialPort openPort(final String appName, final String wantedPortName,
                            final int baudrate, final int databits,
-                           final int stopbits, final int parity) {
+                           final int stopbits, final int parity, final int timeout) {
         return (SerialPort) AccessController.doPrivileged(new PrivilegedAction() {
           public Object run() {
             SerialPort serialPort = null;
             // code to determine portId by enumerating the ports:
-                    CommPortIdentifier portId = null;
-                    Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+            CommPortIdentifier portId = null;
+            Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
 
-                    // iterate through, looking for the port
-                    while (portEnum.hasMoreElements()) {
-                            CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
-                            for (String portName : PORT_NAMES) {
-                                    if (currPortId.getName().equals(portName)) {
-                                            portId = currPortId;
-                                            break;
-                                    }
+            // iterate through, looking for the port
+            while (portEnum.hasMoreElements()) {
+                    CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+                    for (String portName : PORT_NAMES) {
+                            if (currPortId.getName().equals(portName)) {
+                                    portId = currPortId;
+                                    break;
                             }
                     }
+            }
 
+            if(null==portId){                                                   // Failed to find a plugged-in Arduino
+                return null;
+            }        
+                    
             // assuming you have the portId now:
             try {
                 serialPort = (SerialPort) portId.open(
@@ -99,12 +115,88 @@ public class Arduscoplet {
     private static void createAndShowGUI() {
         System.out.println("Created GUI on EDT? "+
         SwingUtilities.isEventDispatchThread());
-        JFrame f = new JFrame("Arduino Oscilloscope, by MakeToLearn");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-        f.add(new MyPanel());
-        f.pack();
-        f.setVisible(true);
-    } 
+        Arduscoplet a = new Arduscoplet();
+        
+        a.f = new JFrame("Arduino Oscilloscope, by MakeToLearn");
+        a.p = new MyPanel();
+        a.f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        a.f.add(a.p);
+        a.f.pack();
+        a.f.setVisible(true);
+
+        a.serialPort = a.openPort("oscilloscope",                               
+                                  "COM3",DATA_RATE,
+                                  SerialPort.DATABITS_8,
+                                  SerialPort.STOPBITS_1,
+                                  SerialPort.PARITY_NONE,
+                                  a.TIME_OUT);
+        if(null==a.serialPort){                                                 // failed to find device
+            System.out.println("Error: Could not find attached Arduino");
+            a.p.showMessage("Could not find attached Arduino");
+        } else {                                                                // success.  device found
+            try {
+                a.in = a.serialPort.getInputStream();
+            } catch(java.io.IOException jioe){
+                System.out.println("Failed to get input stream... \n" + jioe);
+            }
+            
+            try {
+                a.out = a.serialPort.getOutputStream();
+            } catch(java.io.IOException jioe){
+                System.out.println("Failed to get output stream... \n" + jioe);
+            }
+            
+            // add event listeners
+            try {
+                a.serialPort.addEventListener(a);
+            } catch(java.util.TooManyListenersException tmle) {
+                System.out.println("Failed to add listener ... \n" + tmle);
+            }
+            a.serialPort.notifyOnDataAvailable(true);
+            
+        }
+        
+    }
+    
+    public synchronized void serialEvent(SerialPortEvent oEvent) {
+        String eventType = "";
+        switch (oEvent.getEventType()){
+            case SerialPortEvent.BI : eventType = "break interrupt"; break;
+            case SerialPortEvent.CD : eventType = "carrier detect"; break;
+            case SerialPortEvent.CTS: eventType = "clear to send"; break;
+            case SerialPortEvent.DATA_AVAILABLE : eventType = "data available"; break;
+            case SerialPortEvent.DSR : eventType = "data set ready"; break;
+            case SerialPortEvent.FE : eventType = "framing error"; break;
+            case SerialPortEvent.OE : eventType = "overrun error"; break;
+            case SerialPortEvent.OUTPUT_BUFFER_EMPTY : eventType = "output buffer empty"; break;
+            case SerialPortEvent.PE : eventType = "parity error"; break;
+            case SerialPortEvent.RI : eventType = "ring indicator"; break;
+        }
+        p.showMessage("got something... " + eventType);
+        if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+                try {
+                        int available = in.available();
+                        byte chunk[] = new byte[available];
+                        in.read(chunk, 0, available);
+
+                        // Displayed results are codepage dependent
+                        // System.out.print(new String(chunk)); //  print this (PM)
+
+                        System.out.print(Integer.toHexString(0xFFFF & chunk[0])); //  print this (PM)
+                        System.out.print(" ");                          //  print this (PM)
+                        
+                        p.showDataMessage(Integer.toHexString(0xFFFF & chunk[0]));
+
+                } catch (Exception e) {
+                        System.err.println(e.toString());
+                }
+        } else {
+            System.out.println("Received unknown Serial Event Type ...");
+        }
+        // Ignore all the other eventTypes, but you should consider the other ones.
+        
+    }
+    
 }
 
 class MyPanel extends JPanel {
@@ -113,7 +205,10 @@ class MyPanel extends JPanel {
     private int squareY = 50;
     private int squareW = 20;
     private int squareH = 20;
-
+    private String message = "";
+    private String dataMessage = "";
+    private Color currentColor = Color.BLACK;
+    
     public MyPanel() {
 
         setBorder(BorderFactory.createLineBorder(Color.black));
@@ -150,13 +245,37 @@ class MyPanel extends JPanel {
         return new Dimension(450,400);
     }
     
+    public void showMessage(String inMessage){
+        message = inMessage;
+        repaint();
+    }
+    
+    public synchronized void showDataMessage(String inMessage) {
+        if(dataMessage.length() < 50 ) {
+            currentColor = Color.GRAY;                                          // erase the original message
+            repaint();
+            
+            dataMessage = ": " + inMessage + dataMessage;                       // update and draw new message
+//          dataMessage = ": " + inMessage.length() + dataMessage;            // this demonstrated that only one character is being sent...            
+            currentColor = Color.BLACK;
+            repaint();
+        }
+    }
+    
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);       
         g.drawString("Arduino Oscilloscope, by MakeToLearn",10,20);
+        g.drawString(message, 10, 38);
+        g.drawString(dataMessage, 10, 56);
         // g.setColor(Color.RED);
         // g.fillRect(squareX,squareY,squareW,squareH);
         g.setColor(Color.BLACK);
         //g.drawRect(squareX,squareY,squareW,squareH);
         g.drawLine(0,420/2,450,420/2);
+        
+        g.setColor(currentColor);
+        g.drawString(message, 10, 38);
+        g.drawString(dataMessage, 10, 56);
+        
     }  
 }
